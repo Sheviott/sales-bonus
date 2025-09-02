6340b1ee8898119a03a889f43d22cca8587689fb
Vera Rad <game.ru2012@yandex.ru>
2025-09-02 14:36:44 +0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+https://github.com/Sheviott/sales-bonus
 # Проектная работа "Пряники"
 5 спринт, модуль JS
 
--- src/main.js
+++ src/main.js
@@ -1,44 +1,101 @@
-/**
- * Функция для расчета выручки
- * @param purchase запись о покупке
- * @param _product карточка товара
- * @returns {number}
- */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+  const { discount, sale_price, quantity } = purchase;
+
+  return sale_price * quantity * (1 - purchase.discount / 100);
 }
 
-/**
- * Функция для расчета бонусов
- * @param index порядковый номер в отсортированном массиве
- * @param total общее число продавцов
- * @param seller карточка продавца
- * @returns {number}
- */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+  const { profit } = seller;
+
+  if (index == 0) {
+    return (15 / 100) * profit;
+  } else if (index == 1 || index == 2) {
+    return (10 / 100) * profit;
+  } else if (index == total - 1) {
+    return 0;
+  } else {
+    return (5 / 100) * profit;
+  }
 }
 
-/**
- * Функция для анализа данных продаж
- * @param data
- * @param options
- * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
- */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+  if (!data || !Array.isArray(data.sellers) || data.sellers.length === 0) {
+    throw new Error("Некорректные входные данные");
+  }
 
-    // @TODO: Проверка наличия опций
+  const { calculateRevenue, calculateBonus } = options;
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+  if (!calculateRevenue || !calculateBonus) {
+    throw new Error("Чего-то не хватает");
+  }
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+  const sellerStats = data.sellers.map((seller) => ({
+    id: seller.id,
+    name: `${seller.first_name} ${seller.last_name}`,
+    revenue: 0,
+    profit: 0,
+    sales_count: 0,
+    products_sold: {},
+  }));
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+  const sellerIndex = sellerStats.reduce(
+    (result, item) => ({
+      ...result,
+      [item.id]: item,
+    }),
+    {}
+  );
+  const productIndex = data.products.reduce(
+    (result, item) => ({
+      ...result,
+      [item.sku]: item,
+    }),
+    {}
+  );
 
-    // @TODO: Сортировка продавцов по прибыли
+  data.purchase_records.forEach((record) => {
+    const seller = sellerIndex[record.seller_id];
 
-    // @TODO: Назначение премий на основе ранжирования
+    seller.sales_count = (seller.sales_count || 0) + 1;
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+    seller.revenue = (seller.revenue || 0) + record.total_amount;
+
+    record.items.forEach((item) => {
+      const product = productIndex[item.sku];
+
+      const cost = product.purchase_price * item.quantity;
+
+      const revenue = calculateRevenue(item);
+
+      seller.profit += revenue - cost;
+
+      if (!seller.products_sold[item.sku]) {
+        seller.products_sold[item.sku] = 0;
+      }
+      seller.products_sold[item.sku] += item.quantity;
+    });
+  });
+
+  sellerStats.sort((a, b) => b.profit - a.profit);
+
+  sellerStats.forEach((seller, index) => {
+    seller.bonus = calculateBonus(index, sellerStats.length, seller);
+    seller.top_products = Object.entries(seller.products_sold);
+    seller.top_products = seller.top_products.map((item, index) => {
+      return { sku: item[0], quantity: item[1] };
+    }); // Формируем топ-10 товаров
+    seller.top_products = seller.top_products
+      .sort((a, b) => b.quantity - a.quantity)
+      .slice(0, 10);
+  });
+
+  return sellerStats.map((seller) => ({
+    seller_id: seller.id,
+    name: seller.name,
+    revenue: +seller.revenue.toFixed(2),
+    profit: +seller.profit.toFixed(2),
+    sales_count: seller.sales_count,
+    top_products: seller.top_products,
+    bonus: +seller.bonus.toFixed(2),
+  }));
 }
